6b3c2bffe83e233f8a839c57947254582eb30c5d
Lincoln Marler <141679750+lincmarler@users.noreply.github.com>
2023-09-12 16:08:59 -0600

    Tuesday

--- src/services/AxiosService.js
+++ src/services/AxiosService.js
@@ -1,6 +1,18 @@
 import Axios from 'axios'
 import { baseURL } from '../env'
 import { logger } from '../utils/Logger.js'
+import axios from 'axios'
+
+export const giftApi = axios.create({
+  baseURL: 'https://sandbox.codeworksacademy.com/',
+  timeout: 8000
+})
+
+export const giphyApi = axios.create({
+  baseURL: 'https://developers.giphy.com/',
+  timeout: 8000,
+})
+
 
 export const api = Axios.create({
   baseURL,
@@ -18,9 +30,9 @@ function handleAxiosError(error) {
   } else if (error.request) {
     // The request was made but no response was received
     logger.warn('[📡 AXIOS_ERROR_NO_RESPONSE]', error.request)
-  }else {
+  } else {
     // Something happened in setting up the request that triggered an Error
-    logger.warn('[📡 AXIOS_ERROR_INVALID_REQUEST]',error.message)
+    logger.warn('[📡 AXIOS_ERROR_INVALID_REQUEST]', error.message)
   }
   return Promise.reject(error)
 }
